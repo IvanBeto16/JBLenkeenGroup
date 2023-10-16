9058db3686dd1abd1e6783e81bdb733dd55edc5b
JustinoAT <jus.delatorre48@gmail.com>
2023-10-16 15:36:43 -0600

    GetAll

--- PL/Scripts/JavaScript/Empleado.js
+++ PL/Scripts/JavaScript/Empleado.js
@@ -1,1 +1,35 @@
 ﻿//Archivo de funciones para el CRUD de empleado
+$(document).ready(function () { //click
+    GetAll();
+   
+});
+
+
+function GetAll() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:57440/api/Empleado/GetAll',
+        success: function (result) { //200 OK 
+            $.each(result.Objects, function (empleado) {
+                var filas =
+                    '<tr>'
+                    + '<td class="text-center"> '
+                    + '<a href="#" onclick="GetById(' + empleado.IdEmpleado + ')">'
+                    + '<img  style="height: 25px; width: 25px;" src="../img/edit.ico" />'
+                    + '</a> '
+                    + '</td>'
+                    + "<td  id='id' class='text-center'>" + empleado.Nombre + "</td>"
+                    + "<td class='text-center'>" + empleado.ApellidoPaterno + "</td>"
+                    + "<td class='text-center'>" + empleado.ApellidoMaterno + "</ td>"
+                    + "<td class='text-center'>" + empleado.IdEntidad + "</td>"
+                    + '<td class="text-center"> <button class="btn btn-danger" onclick="Eliminar(' + empleado.IdEmpleado + ')"><span class="glyphicon glyphicon-trash" style="color:#FFFFFF"></span></button></td>'
+
+                    + "</tr>";
+                $("#Empleados tbody").append(filas);
+            });
+        },
+        error: function (result) {
+            alert('Error en la consulta.' + result.responseJSON.ErrorMessage);
+        }
+    });
+};
