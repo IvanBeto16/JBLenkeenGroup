a4ae2288d2cdb76482f6c2cf506099a6b6b0f8ce
IvanBeto16 <ivan16beto@gmail.com>
2023-10-16 13:00:20 -0600

    Servicios Add y Update Empleado

--- SL/Controllers/EmpleadoController.cs
+++ SL/Controllers/EmpleadoController.cs
@@ -10,8 +10,36 @@ namespace SL.Controllers
     [RoutePrefix("api/Empleado")]
     public class EmpleadoController : ApiController
     {
-       
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult Add([FromBody]ML.Empleado empleado)
+        {
+            ML.Result result = BL.Empleado.Add(empleado);
+            if (result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
 
+        [Route("{idEmpleado}")]
+        [HttpPut]
+        public IHttpActionResult Update(int idEmpleado, [FromBody]ML.Empleado empleado)
+        {
+            empleado.IdEmpleado = idEmpleado;
+            ML.Result result = BL.Empleado.Update(empleado);
+            if(result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
 
 
 
